9325e32fea4b6e62f6bdcecde7e345d052c4646f
beyond-suraj <suraj.rai@beyondhimalayatech.com.au>
2025-10-09 11:14:04 +0545

    removed proxyURL

--- src/lib/api/lotApi.ts
+++ src/lib/api/lotApi.ts
@@ -140,8 +140,7 @@ export const getImageUrlWithCorsProxy = (imagePath: string | null | undefined): 
   if (!imagePath) return '';
   
   if (imagePath.startsWith('http://') || imagePath.startsWith('https://')) {
-    const proxyUrl = 'https://corsproxy.io/'; 
-    return `${proxyUrl}?${encodeURIComponent(imagePath)}`;
+    return imagePath;
   }
   
   return imagePath;
